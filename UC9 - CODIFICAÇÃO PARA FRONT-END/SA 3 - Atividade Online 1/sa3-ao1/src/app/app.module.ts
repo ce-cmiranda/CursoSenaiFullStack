627200e9c3c77633b9b7844b3954e168f44137ef
Carlos Eduardo Miranda <ce.carv.miranda@gmail.com>
2022-12-11 13:40:14 -0300

    SA3 - AO1 Angular Project

---
+++ UC9 - CODIFICAÇÃO PARA FRONT-END/SA 3 - Atividade Online 1/sa3-ao1/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { ConstrucaoComponent } from './views/construcao/construcao.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { DestaquesComponent } from './components/destaques/destaques.component';
+import { OfertasComponent } from './components/ofertas/ofertas.component';
+
+
+
+// Form
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ConstrucaoComponent,
+    NotfoundComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    DestaquesComponent,
+    OfertasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ UC9 - CODIFICAÇÃO PARA FRONT-END/SA 3 - Atividade Online 1/sa3-ao1/src/app/components/ofertas/ofertas.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ofertas',
+  templateUrl: './ofertas.component.html',
+  styleUrls: ['./ofertas.component.css']
+})
+export class OfertasComponent implements OnInit {
+
+ 
+  cards = [0,1,2,3,4,5];
+  
+  constructor() {
+   }
+
+
+
+
+
+
+  ngOnInit(): void {
+  }
+
+}
