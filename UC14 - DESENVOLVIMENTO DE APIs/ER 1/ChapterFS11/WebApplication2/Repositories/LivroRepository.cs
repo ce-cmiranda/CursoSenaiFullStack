3c156fb0df27ff66c0278f11fc88169cec6011d0
Carlos Eduardo de Carvalho Miranda <carlos.miranda@sesisenaisp.org.br>
2023-02-22 10:14:58 -0300

    UC14 ER1 - Método GET

---
+++ UC14 - DESENVOLVIMENTO DE APIs/ER 1/ChapterFS11/WebApplication2/Repositories/LivroRepository.cs
@@ -0,0 +1,18 @@
+﻿using ChapterFS11.Contexts;
+using ChapterFS11.Models;
+
+namespace ChapterFS11.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly SQLcontext _context;
+        public LivroRepository(SQLcontext context) 
+        {
+            _context = context;
+        }
+        public List<Livro> GetAll()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
