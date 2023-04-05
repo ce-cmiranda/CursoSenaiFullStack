a89d3a912088ce84e5a53624b9d7fae1c3e9fed1
Carlos Miranda <ce.carv.miranda@gmail.com>
2023-04-04 21:22:21 -0300

    UC14 ER3 git ignore

--- UC14 - DESENVOLVIMENTO DE APIs/ER 3/ChapterFS11/WebApplication2/Controllers/UsuarioController.cs
+++ UC14 - DESENVOLVIMENTO DE APIs/ER 3/ChapterFS11/WebApplication2/Controllers/UsuarioController.cs
@@ -84,55 +84,5 @@ namespace ChapterFS11.Controllers
                 throw new Exception(e.Message);
             }
         }
-
-
-        /*        [HttpPost]
-
-                public IActionResult Login(LoginViewModel dadosLogin)
-                {
-                    try
-                    {
-                        Usuario usuarioBuscado = _iUsuarioRepository.Login(dadosLogin?.email!, dadosLogin?.senha!)!;
-
-                        if (usuarioBuscado == null)
-                        {
-                            return Unauthorized(new { msg = "Email ou Senha incorreto" });
-                        }
-
-                        //inicio da configuração do token
-                        var minhasClaims = new[]
-                        {
-                            new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado ?.Email!),
-                            new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado?.id.ToString()!),
-                            new Claim(ClaimTypes.Role, usuarioBuscado ?.Tipo!)
-                        };
-
-                        var chave = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("chapter-chave-autenticacao"));
-
-                        var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
-
-                        var meuToken = new JwtSecurityToken(
-
-                            issuer: "chapter.webapi",
-                            audience: "chapter.webapi",
-                            claims: minhasClaims,
-                            expires: DateTime.Now.AddMinutes(60),
-                            signingCredentials: credenciais
-                            );
-
-                        return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(meuToken) });
-
-                    }
-                    catch (Exception e)
-                    {
-                        throw new Exception(e.Message);
-                    }
-
-                }*/
-
-
-
-
-
     }
 }
